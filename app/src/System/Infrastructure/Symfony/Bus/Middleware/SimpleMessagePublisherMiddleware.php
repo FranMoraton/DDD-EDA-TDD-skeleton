a0634d844132a36cfe6e57f50345173dfa1f0dbe
Fran Moraton <franciscoa.moraton@pccomponentes.com>
2024-10-08 15:06:44 +0200

    add messenger event bus and comand puslisher middleware

---
+++ app/src/System/Infrastructure/Symfony/Bus/Middleware/SimpleMessagePublisherMiddleware.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace App\System\Infrastructure\Symfony\Bus\Middleware;
+
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
+use Symfony\Component\Messenger\Middleware\StackInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
+
+final class SimpleMessagePublisherMiddleware implements MiddlewareInterface
+{
+    public function __construct(private MessageBusInterface $asyncCommandBus)
+    {
+    }
+
+    public function handle(Envelope $envelope, StackInterface $stack): Envelope
+    {
+        $resultStack = $stack->next()->handle($envelope, $stack);
+        $commandsResult = $this->extractHandledStamp($resultStack);
+
+        if ((\is_countable($commandsResult) && 0 === \count($commandsResult))) {
+            return $resultStack;
+        }
+
+        foreach ($commandsResult as $commands) {
+            if (null === $commands) {
+                continue;
+            }
+            // We can accept both one or an array of commands to be dispatched
+            if (false === \is_array($commands)) {
+                $commands = [$commands];
+            }
+
+            foreach ($commands as $theCommand) {
+                if (false === \is_object($theCommand)) {
+                    continue;
+                }
+
+                $this->asyncCommandBus->dispatch($theCommand);
+            }
+        }
+
+        return $resultStack;
+    }
+
+    private function extractHandledStamp(Envelope $envelope): array
+    {
+        $results = [];
+
+        foreach ($envelope->all() as $key => $stamp) {
+            if (HandledStamp::class !== $key) {
+                continue;
+            }
+
+            foreach ($stamp as $resultStamp) {
+                \assert($resultStamp instanceof HandledStamp);
+                $results[] = $resultStamp->getResult();
+            }
+        }
+
+        return $results;
+    }
+}
