151943b54e872cebc9e916734c16fa7671acc66d
Fran Moraton <franciscoa.moraton@pccomponentes.com>
2024-10-08 19:59:29 +0200

    add transactional lock fields migration

---
+++ app/migrations/Version20241008175819.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20241008175819 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'add transactional fields to lock models';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('
+            ALTER TABLE factions 
+            ADD updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL, 
+            ADD version INT NOT NULL DEFAULT 1;
+        ');
+
+        $this->addSql('
+            ALTER TABLE equipments 
+            ADD updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL, 
+            ADD version INT NOT NULL DEFAULT 1;
+        ');
+
+        $this->addSql('
+            ALTER TABLE characters 
+            ADD updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL, 
+            ADD version INT NOT NULL DEFAULT 1;
+        ');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE factions DROP COLUMN updated_at, DROP COLUMN version;');
+        $this->addSql('ALTER TABLE equipments DROP COLUMN updated_at, DROP COLUMN version;');
+        $this->addSql('ALTER TABLE characters DROP COLUMN updated_at, DROP COLUMN version;');
+    }
+}
