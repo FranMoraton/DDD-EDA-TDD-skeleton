24ea2f1738d16efbb47d7ca2e71e193921d5b631
Fran Moraton <franciscoa.moraton@pccomponentes.com>
2024-10-08 11:58:12 +0200

    migrations

---
+++ app/migrations/Version20241008090627.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20241008090627 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Event Storage';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('
+            CREATE TABLE IF NOT EXISTS event_store (
+                message_id CHAR(36) NOT NULL PRIMARY KEY,
+                aggregate_id VARCHAR(36) NOT NULL,
+                message_name VARCHAR(255) NOT NULL,
+                payload TEXT NOT NULL,
+                occurred_on DECIMAL(18, 6) NOT NULL,
+                aggregate_version VARCHAR(60) NOT NULL
+            );
+        ');
+
+        $this->addSql('CREATE INDEX event_store_aggregate_id_idx ON event_store(aggregate_id);');
+        $this->addSql('CREATE INDEX event_store_message_name_idx ON event_store(message_name);');
+        $this->addSql('CREATE INDEX event_store_occurred_on_idx ON event_store(occurred_on);');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('drop table event_store;');
+    }
+}
---
+++ app/migrations/Version20241008094742.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20241008094742 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE DATABASE IF NOT EXISTS `lotr` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;');
+        $this->addSql('USE `lotr`;');
+
+        $this->addSql('
+            CREATE TABLE IF NOT EXISTS `factions` (
+                `id` INT NOT NULL AUTO_INCREMENT,
+                `faction_name` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+                `description` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+                PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+        ');
+
+        $this->addSql('
+            CREATE TABLE IF NOT EXISTS `equipments` (
+                `id` INT NOT NULL AUTO_INCREMENT,
+                `name` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+                `type` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+                `made_by` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+                PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+        ');
+
+        $this->addSql('
+            CREATE TABLE IF NOT EXISTS `characters` (
+                `id` INT NOT NULL AUTO_INCREMENT,
+                `name` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+                `birth_date` DATE NOT NULL,
+                `kingdom` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+                `equipment_id` INT NOT NULL,
+                `faction_id` INT NOT NULL,
+                PRIMARY KEY (`id`),
+                KEY `equipment_id` (`equipment_id`),
+                KEY `faction_id` (`faction_id`),
+                CONSTRAINT `characters_ibfk_1` FOREIGN KEY (`equipment_id`) REFERENCES `equipments` (`id`),
+                CONSTRAINT `characters_ibfk_2` FOREIGN KEY (`faction_id`) REFERENCES `factions` (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+        ');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE IF EXISTS `factions`;');
+        $this->addSql('DROP TABLE IF EXISTS `equipments`;');
+        $this->addSql('DROP TABLE IF EXISTS `characters`;');
+        $this->addSql('DROP DATABASE IF EXISTS `lotr`;');
+    }
+}
